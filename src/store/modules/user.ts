f89d71b6308f60d71b925367782cfc4c27e5e375
A-Juran <2369668922@qq.com>
2023-06-26 12:31:19 +0800

    feat: Login Secondary Encapsulation(token type)

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,27 +1,29 @@
 //create user related warehouses
 import { defineStore } from 'pinia'
 import { reqLogin } from '@/api/user'
-import { loginForm } from '@/api/user/type'
+import type { loginForm, loginReturnsData } from '@/api/user/type'
+import type { UserState } from './types/types'
+import { SET_TOKEN, GET_TOKEN } from '@/utils/token'
+
 //create user small warehouses
 const useUserStore = defineStore('User', {
   //small warehouses
-  state: () => {
+  state: (): UserState => {
     return {
-      token: localStorage.getItem('token'),
+      token: GET_TOKEN(),
     }
   },
   //
   actions: {
     async userLogin(data: loginForm) {
-      const result = await reqLogin(data)
-      console.log(result.data)
+      const result: loginReturnsData = await reqLogin(data)
       //login fail
       if (result.code == 201) {
         return Promise.reject(new Error(result.data.message))
       }
       //login success
-      this.token = result.data.token
-      localStorage.setItem('token', result.data.token)
+      this.token = result.data.token as string
+      SET_TOKEN(result.data.token as string)
       return Promise.resolve()
     },
   },
